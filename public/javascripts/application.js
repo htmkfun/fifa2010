325a6d0722ac300850a2398ee488bfeee8165914
RainChen <hirainchen@gmail.com>
2010-06-10 22:37:48 +0800

    setup base html stuff

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,36 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function(){
+  
+  $("#groups").each(function(){
+    var groupBoxTpl = $("#team-model").html();
+    var teamBoxTpl = $("#team-model").find(".teams").html();
+    var groupsBox = $(this);
+    groupsBox.html("");
+
+    // init html
+    for(var i="A".charCodeAt();i <= "H".charCodeAt();i++){
+      var groupNo = String.fromCharCode(i);
+      var groupBox = $(groupBoxTpl).appendTo(groupsBox);
+      groupBox.find("h2").html("Group "+groupNo);
+      groupBox.find(".teams").html("");
+      for(var j=1;j<=4;j++){
+        var teamBox = $(teamBoxTpl).appendTo(groupBox.find(".teams"));
+        teamBox.find(".team").html("Group "+groupNo+" Team "+j);
+      }
+    }
+
+    // init groupsBox behaviors
+    groupsBox.find(".teams").sortable({
+      cursor: 'crosshair'
+    });
+
+    // init koboardBox
+    $("#koboard .fteam").hover(function(){
+      $(this).addClass('mouseover');
+    }, function(){
+      $(this).removeClass('mouseover');
+    })
+
+  });
+
+
+});
