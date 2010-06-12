9b5d9cd82da815749de9e61cfd0a2d4f4af1bae5
RainChen <hirainchen@gmail.com>
2010-06-12 18:34:29 +0800

    Resolved #651: make group sortable

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,16 @@
 $(function(){
-  
+
+  // init datas
+  var teamGroups = {}; // { A: [{name: "T1", tag: "T1"}, '{name: "T2", tag: "T2"}, {name: "T3", tag: "T3"}, '{name: "T4", tag: "T4"}], B;[...] }
+  // prepare fake data
+  for(var i="A".charCodeAt();i <= "H".charCodeAt();i++){
+    var groupNo = String.fromCharCode(i);
+    teamGroups[groupNo] = [];
+    for(var j=1;j<=4;j++){
+      teamGroups[groupNo].push( {name:("Group "+groupNo+" Team "+j), tag: ("g_"+groupNo+"_t_"+j)} );
+    }
+  }
+
   $("#groups").each(function(){
     var groupBoxTpl = $("#team-model").html();
     var teamBoxTpl = $("#team-model").find(".teams").html();
@@ -7,19 +18,19 @@ $(function(){
     groupsBox.html("");
 
     // init html
-    for(var i="A".charCodeAt();i <= "H".charCodeAt();i++){
-      var groupNo = String.fromCharCode(i);
+    $.each(teamGroups, function(groupNo, teams){
       var groupBox = $(groupBoxTpl).appendTo(groupsBox);
       groupBox.find("h2").html("Group "+groupNo);
       groupBox.find(".teams").html("");
-      for(var j=1;j<=4;j++){
+      $.each(teams, function(i, team){
         var teamBox = $(teamBoxTpl).appendTo(groupBox.find(".teams"));
-        teamBox.find(".team").html("Group "+groupNo+" Team "+j);
-      }
-    }
+        teamBox.find(".team").html(team.name);
+      });
+    });
 
     // init groupsBox behaviors
     groupsBox.find(".teams").sortable({
+      containment: 'parent',
       cursor: 'crosshair'
     });
 
@@ -28,7 +39,7 @@ $(function(){
       $(this).addClass('mouseover');
     }, function(){
       $(this).removeClass('mouseover');
-    })
+    });
 
   });
 
